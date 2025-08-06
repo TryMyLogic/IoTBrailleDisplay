22f3e38ba80e763093ae8c70d16a57c1bb8d2361
zannlin <zanderlindeque1@gmail.com>
2025-08-06 12:30:20 +0200

    Fix Previous merge conflict

--- DisplayLogic.Tests/Services/IoTDeviceTests/IoTDeviceTests.cs
+++ DisplayLogic.Tests/Services/IoTDeviceTests/IoTDeviceTests.cs
@@ -8,35 +8,6 @@ namespace DisplayLogic.Tests.Services.IoTDeviceTests
         private const string TestRestEndpoint = "http://localhost/api";
 
         [SkippableFact]
-        public async Task Should_Connect_To_Mqtt_Broker()
-        {
-            IoTDevice device = new(TestBroker, TestRestEndpoint);
-            await device.ConnectAsync();
-
-            Assert.True(device.IsConnected);
-        }
-
-        [SkippableFact]
-        public async Task Should_Publish_And_Receive_Message()
-        {
-            string topic = "iot/test/message";
-            string expectedPayload = "Hello World";
-
-            IoTDevice device = new(TestBroker, TestRestEndpoint);
-            await device.ConnectAsync();
-
-            // Use Task.Run to simulate publish delay
-            _ = Task.Run(async () =>
-            {
-                await Task.Delay(1000);
-                await device.PublishAsync(topic, expectedPayload);
-            });
-
-            string received = await device.SubscribeAsync(topic);
-            Assert.Equal(expectedPayload, received);
-        }
-
-        [SkippableFact]
         public async Task Publish_Should_Throw_If_Not_Connected()
         {
             IoTDevice device = new(TestBroker, TestRestEndpoint);
